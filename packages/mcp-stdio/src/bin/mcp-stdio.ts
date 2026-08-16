a89e771acffd4e71060cbc35413ae427292c3c84
Richard Ng <33248610+richardcrng@users.noreply.github.com>
2026-08-16 10:10:28 +0100

    Split Native package from marketplace (#7)

--- packages/mcp-stdio/src/bin/mcp-stdio.ts
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-/* This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. */
-import { run } from '../runner.js'
-process.exitCode = await run('proxy')
